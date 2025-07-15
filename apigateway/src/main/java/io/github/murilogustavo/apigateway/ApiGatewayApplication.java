fdfd535d467842256d8ae04961a4fb5b12437538
MuriloGustavo <murilogustavo98@gmail.com>
2025-07-15 14:45:23 -0300

    add api gateway

---
+++ apigateway/src/main/java/io/github/murilogustavo/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package io.github.murilogustavo.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("clients", r -> r.path("/clients/**").uri("lb://client-service"))
+				.build();
+	}
+}
